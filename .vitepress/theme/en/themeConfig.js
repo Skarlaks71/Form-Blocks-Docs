bf94be7106fc7fa1332ba72ce62f3347512c9ff8
Matheus Pinheiro <mapisaro07@gmail.com>
2026-05-24 17:11:30 -0300

    traduções finalizadas

--- .vitepress/theme/en/themeConfig.js
+++ .vitepress/theme/en/themeConfig.js
@@ -19,38 +19,38 @@ export const englishThemeConfig = {
           text: 'Blocks',
           collapsed: true,
           items: [
-            { text: 'FormBlocks', link: '/en/components/blocks/form-blocks' },
-            { text: 'FormGroupBlocks', link: '/en/components/blocks/form-group-blocks' },
-            { text: 'FormInputsBlocks', link: '/en/components/blocks/form-inputs-blocks' },
-            { text: 'FormBlocksRepeater', link: '/en/components/blocks/form-blocks-repeater' },
-            { text: 'FormInputsRepeaterItem', link: '/en/components/blocks/form-blocks-repeater-item' },
+            { text: 'FormBlocks', link: '/en/guide/blocks/form-blocks' },
+            { text: 'FormGroupBlocks', link: '/en/guide/blocks/form-group-blocks' },
+            { text: 'FormInputsBlocks', link: '/en/guide/blocks/form-inputs-blocks' },
+            { text: 'FormBlocksRepeater', link: '/en/guide/blocks/form-blocks-repeater' },
+            { text: 'FormInputsRepeaterItem', link: '/en/guide/blocks/form-blocks-repeater-item' },
           ]
         },
         {
           text: 'Layout',
           collapsed: true,
           items: [
-            { text: 'Container', link: '/en/components/layout/fb-container' },
-            { text: 'Row', link: '/en/components/layout/fb-row' },
-            { text: 'Col', link: '/en/components/layout/fb-col' },
+            { text: 'Container', link: '/en/guide/layout/fb-container' },
+            { text: 'Row', link: '/en/guide/layout/fb-row' },
+            { text: 'Col', link: '/en/guide/layout/fb-col' },
           ]
         },
         {
           text: 'Inputs',
           collapsed: true,
           items: [
-            { text: 'Input', link: '/en/components/inputs/fb-input' },
-            { text: 'Input Block', link: '/en/components/inputs/fb-input-block' },
-            { text: 'Textarea', link: '/en/components/inputs/fb-textarea' },
-            { text: 'Checkbox', link: '/en/components/inputs/fb-checkbox' },
-            { text: 'Radio', link: '/en/components/inputs/fb-radio' },
+            { text: 'Input', link: '/en/guide/inputs/fb-input' },
+            { text: 'Input Block', link: '/en/guide/inputs/fb-input-block' },
+            { text: 'Textarea', link: '/en/guide/inputs/fb-textarea' },
+            { text: 'Checkbox', link: '/en/guide/inputs/fb-checkbox' },
+            { text: 'Radio', link: '/en/guide/inputs/fb-radio' },
           ]
         },
         {
           text: 'Miscellaneous',
           collapsed: true,
           items: [
-            { text: 'Button', link: '/en/components/miscellaneous/fb-button' },
+            { text: 'Button', link: '/en/guide/miscellaneous/fb-button' },
           ]
         },
       ],
